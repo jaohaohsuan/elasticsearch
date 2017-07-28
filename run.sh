5d38320014a1857499bca8a39a84671827341ffe
Henry Jao <jaohaohsuan@gmail.com>
2017-07-28 12:01:20 +0800

    change permission of es.path.data

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 set -e
 ulimit -l unlimited
 export ES_JAVA_OPTS="-Des.cgroups.hierarchy.override=/ $ES_JAVA_OPTS"
+if [[ -d ${ES_PATH_DATA} ]]; then
+	chown elasticsearch.elasticsearch ${ES_PATH_DATA}
+fi
 exec su -m elasticsearch -c "$*"
